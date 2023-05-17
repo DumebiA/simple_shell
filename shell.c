65db2fa25ed7ee24d54dac4ef8e7d89cae7c674a
DumebiA <anyamkpa.ifeanyi@gmail.com>
2023-05-17 10:04:07 -0700

    corrected error and split files

--- shell.c
+++ shell.c
@@ -1,64 +1,94 @@
 #include "main.h"
 
-int main(int argc, char **argv) {
-	char *prompt = "Myshell $ ";
-	char *lineptr = NULL;
-	char *line_copy = NULL;
-	size_t x = 0;
-	ssize_t nread;
+/**
+ * main - Simple Shell (Hsh)
+ * @argc: Argument Count
+ * @argv:Argument Value
+ * Return: Exit Value By Status
+ */
+
+void parseInput(char *lineptr, char ***argv)
+{
 	const char *delim = " \n";
 	int num_tokens = 0;
+	char *line_copy = NULL;
 	char *toks;
-	int pipe = 0;
 	int n;
 
-	(void)argc;
+	toks = strtok(lineptr, delim);
 
-	while (1 && !pipe) {
+	while (toks != NULL)
+	{
+		num_tokens++;
+		toks = strtok(NULL, delim);
+	}
+	num_tokens++;
+
+	*argv = malloc(sizeof(char *) * num_tokens);
+
+	line_copy = malloc(sizeof(char) * strlen(lineptr));
+	strcpy(line_copy, lineptr);
+
+	toks = strtok(line_copy, delim);
+
+	for (n = 0; toks != NULL; n++)
+	{
+		(*argv)[n] = malloc(sizeof(char) * strlen(toks));
+		strcpy((*argv)[n], toks);
+
+		toks = strtok(NULL, delim);
+	}
+	(*argv)[n] = NULL;
+
+	free(line_copy);
+}
+
+void excmd(char **argv)
+{
+	char *cmd = NULL;
+
+	if (argv)
+	{
+		cmd = argv[0];
+
+		if (execve(cmd, argv, NULL) == -1)
+		{
+			perror("err");
+		}
+	}
+}
+
+int main(int argc, char **argv)
+{
+	char *prompt = "Myshell $ ";
+	char *lineptr = NULL;
+	char **args = NULL;
+	size_t x = 0;
+	ssize_t nread;
+	int pipe = 0;
+
+	(void)argc;
+	(void)argv;
+
+	while (1 && !pipe)
+	{
 		if (isatty(STDIN_FILENO) == 0)
 			pipe = 1;
-
 		write(STDOUT_FILENO, prompt, strlen(prompt));
 		nread = getline(&lineptr, &x, stdin);
 
-		if (nread == -1) {
+		if (nread == -1)
+		{
 			write(STDOUT_FILENO, "exit\n", strlen("exit\n"));
 			return (-1);
 		}
 
-		line_copy = malloc(sizeof(char) * nread);
+		parseInput(lineptr, &args);
 
-		if (line_copy == NULL) {
-			perror("memory allocation err");
-			return (-1);
-		}
-
-		strcpy(line_copy, lineptr);
-
-		toks = strtok(lineptr, delim);
-
-		while (toks != NULL) {
-			num_tokens++;
-			toks = strtok(NULL, delim);
-		}
-		num_tokens++;
-
-		argv = malloc(sizeof(char *) * num_tokens);
-
-		toks = strtok(line_copy, delim);
-
-		for (n = 0; toks != NULL; n++) {
-			argv[n] = malloc(sizeof(char) * (strlen(toks) + 1));
-			strcpy(argv[n], toks);
-
-			toks = strtok(NULL, delim);
-		}
-		argv[n] = NULL;
-
-		excmd(argv);
+		excmd(args);
 	}
 
-	free(line_copy);
+	free(args);
 	free(lineptr);
 
 	return (0);
