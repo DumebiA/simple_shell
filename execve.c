8e086fedffaa68141041c4fd2289f4f664394688
DumebiA <anyamkpa.ifeanyi@gmail.com>
2023-05-24 05:36:19 -0700

    redone

--- execve.c
+++ execve.c
@@ -10,16 +10,13 @@
 void excmd(char **argv)
 {
 	char *cmd = NULL;
-	char *a_cmd = NULL;
 
 	if (argv)
 	{
 		cmd = argv[0];
-		a_cmd = get_location(cmd);
-
-		if (execve(a_cmd, argv, NULL) == -1)
+		if (execve(cmd, argv, NULL) == -1)
 		{
-			perror("error message");
+			perror("./hsh");
 		}
 	}
 }
---
+++ shell.c
@@ -0,0 +1,67 @@
+#include "main.h"
+
+int main(int argc, char **argv)
+{
+    char *prompt = "($) ";
+    char *lineptr = NULL;
+    char *line_copy = NULL;
+    size_t x = 0;
+    ssize_t nread;
+    const char *delim = " \n";
+    int num_tokens = 0;
+    char *toks;
+    int n;
+
+    (void)argc;
+
+    while (1)
+    {
+        write(STDOUT_FILENO, prompt, strlen(prompt));
+        nread = getline(&lineptr, &x, stdin);
+
+        if (nread == -1)
+        {
+            write(STDOUT_FILENO, "exit\n", strlen("exit\n"));
+            return (-1);
+        }
+
+        line_copy = malloc(sizeof(char) * nread);
+
+        if (line_copy == NULL)
+        {
+            perror("err: not found");
+            return (-1);
+        }
+
+        strcpy(line_copy, lineptr);
+
+        toks = strtok(lineptr, delim);
+
+        while (toks != NULL)
+        {
+            num_tokens++;
+            toks = strtok(NULL, delim);
+        }
+        num_tokens++;
+
+        argv = malloc(sizeof(char *) * num_tokens);
+
+        toks = strtok(line_copy, delim);
+
+        for (n = 0; toks != NULL; n++)
+        {
+            argv[n] = malloc(sizeof(char) * strlen(toks));
+            strcpy(argv[n], toks);
+
+            toks = strtok(NULL, delim);
+        }
+        argv[n] = NULL;
+
+        excmd(argv);
+    }
+
+    free(line_copy);
+    free(lineptr);
+
+    return (0);
+}
