0744f10b46f2a00d7020979a50b14b99a361f0f8
DumebiA <anyamkpa.ifeanyi@gmail.com>
2023-05-25 10:03:56 -0700

    push

--- main.c
+++ main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * excmd - function executes stored prompt
  * @command: prompt command storage to be executed
@@ -54,6 +53,40 @@ char** parse(char* command, int* num_args)
 	return args;
 }
 
+/**
+ * excmd - function executes stored prompt
+ * @command: prompt command storage to be executed
+ *
+ * Return: the difference between final value of s and the initial value of str
+ */
+
+void excmd(char** args)
+{
+	pid_t pid;
+
+	pid = fork();
+
+	if (pid < 0)
+	{
+		char error[] = "child process failed\n";
+		write(STDERR_FILENO, error, sizeof(error) - 1);
+		return;
+	}
+
+	if (pid == 0)
+	{
+		if (execve(args[0], args, NULL) == -1)
+		{
+			char error[] = "./hsh: command not found\n";
+			write(STDERR_FILENO, error, sizeof(error) - 1);
+			exit(1);
+		}
+	}
+	else
+	{
+		wait(NULL);
+	}
+}
 
 /**
  * main - prints main function
@@ -97,3 +130,4 @@ int main()
 	}
 	return 0;
 }
+
