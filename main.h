40582572d016e8df546150038fdc0db68b19a3a0
igwexxi <agbodaniel44@gmail.com>
2023-05-25 02:20:21 -0700

    update

--- main.h
+++ main.h
@@ -12,10 +12,14 @@
 
 extern char **environ;
 #define MAX_COMMAND_LENGTH 100
+<<<<<<< HEAD
 void input_prompt(void);
 char *get_line(void);
 void excmd(char *command);
 int main(void);
+=======
+#define MAX_ARGS 10
+>>>>>>> 19c0325598e42686cd97ab2d677f2bc365211832
 
 
 #endif
