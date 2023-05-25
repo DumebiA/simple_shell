51a9a9ddc06eb591ea239257a909aa785cf8909e
DumebiA <anyamkpa.ifeanyi@gmail.com>
2023-05-25 10:13:13 -0700

    push

---
+++ excmd.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * excmd - function executes stored prompt
+ * @command: prompt command storage to be executed
+ *
+ * Return: the difference between final value of s and the initial value of str
+ */
+
+void excmd(char** args)
+{
+	pid_t pid;
+
+	pid = fork();
+
+	if (pid < 0)
+	{
+		char error[] = "child process failed\n";
+		write(STDERR_FILENO, error, sizeof(error) - 1);
+		return;
+	}
+
+	if (pid == 0)
+	{
+		if (execve(args[0], args, NULL) == -1)
+		{
+			char error[] = "./hsh: command not found\n";
+			write(STDERR_FILENO, error, sizeof(error) - 1);
+			exit(1);
+		}
+	}
+	else
+	{
+		wait(NULL);
+	}
+}
--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * excmd - function executes stored prompt
  * @command: prompt command storage to be executed
@@ -51,41 +52,6 @@ char** parse(char* command, int* num_args)
 	*num_args = index;
 
 	return args;
-}
-
-/**
- * excmd - function executes stored prompt
- * @command: prompt command storage to be executed
- *
- * Return: the difference between final value of s and the initial value of str
- */
-
-void excmd(char** args)
-{
-	pid_t pid;
-
-	pid = fork();
-
-	if (pid < 0)
-	{
-		char error[] = "child process failed\n";
-		write(STDERR_FILENO, error, sizeof(error) - 1);
-		return;
-	}
-
-	if (pid == 0)
-	{
-		if (execve(args[0], args, NULL) == -1)
-		{
-			char error[] = "./hsh: command not found\n";
-			write(STDERR_FILENO, error, sizeof(error) - 1);
-			exit(1);
-		}
-	}
-	else
-	{
-		wait(NULL);
-	}
 }
 
 /**
