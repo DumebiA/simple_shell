799f5ad4720587cf4f4e5de25546185dba30ca4a
igwexxi <agbodaniel44@gmail.com>
2023-05-25 18:15:31 -0700

    tired update

--- excmd.c
+++ excmd.c
@@ -2,13 +2,16 @@
 
 /**
  * excmd - function executes stored prompt
- * @command: prompt command storage to be executed
- *
+ * @command: prompt command to be executed
+ * @args: array of string
+ * @argv: command array
  * Return: the difference between final value of s and the initial value of str
  */
 
-void excmd(char** args)
+void excmd(char **args, **argv)
 {
+	char *command = NULL, *actual_command = NULL;
+
 	pid_t pid;
 
 	pid = fork();
@@ -16,15 +19,19 @@ void excmd(char** args)
 	if (pid < 0)
 	{
 		char error[] = "child process failed\n";
+
 		write(STDERR_FILENO, error, sizeof(error) - 1);
 		return;
 	}
 
+	actual_commad = environ(command);
+
 	if (pid == 0)
 	{
-		if (execve(args[0], args, NULL) == -1)
+		if (execve(actual_command, args, NULL) == -1)
 		{
 			char error[] = "./hsh: command not found\n";
+
 			write(STDERR_FILENO, error, sizeof(error) - 1);
 			exit(1);
 		}
--- main.h
+++ main.h
@@ -9,11 +9,15 @@
 #include <sys/stat.h>
 #include <stddef.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #define MAX_COMMAND_LENGTH 100
 #define MAX_ARGS 10
 
 
 void excmd(char **args);
+char *find_executable(const char *command);
+
 
 #endif
