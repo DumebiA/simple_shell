037e88c7579f83cb7a866c6d2d131eb33273d087
DumebiA <anyamkpa.ifeanyi@gmail.com>
2023-05-26 17:17:39 -0700

    pushed

--- management.c
+++ management.c
@@ -1,49 +1,38 @@
-#include "shell.h"
+#include "main.h"
 
-/**
- **_realloc -  Reallocates A Memory Block Using Malloc And Free
- *@ptr: Pointer
- *@old_size: Previous Size Of The Pointer
- *@new_size: New Size Of The Pointer
- *Return: Void Pointer Rellocated Memory
- */
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+void *re_alloc(void *pointr, unsigned int os, unsigned int ns)
 {
-	void *result;
+	void *res;
 
-	if (new_size == old_size)
-		return (ptr);
-	if (new_size == 0 && ptr)
+	if (ns == os)
+		return (pointr);
+	if (ns == 0 && pointr)
 	{
-		free(ptr);
+		free(pointr);
 		return (NULL);
 	}
-	result = malloc(new_size);
-	if (result == NULL)
+	res = malloc(ns);
+	if (res == NULL)
 		return (NULL);
-	if (ptr == NULL)
+	if (pointr == NULL)
 	{
-		fill_an_array(result, '\0', new_size);
-		free(ptr);
+		fill_an_array(res, '\0', ns);
+		free(pointr);
 	}
 	else
 	{
-		_memcpy(result, ptr, old_size);
-		free(ptr);
+		mem_cpy(res, pointr, os);
+		free(pointr);
 	}
-	return (result);
+	return (res);
 
 }
-/**
- * free_all - Free Array Of Char Pointer And Char Pointer
- * @cmd:Array Pointer
- * @line:Char Pointer
- * Return: Void
- */
-void free_all(char **cmd, char *line)
+
+void freeAll(char **cmd, char *line)
 {
 	free(cmd);
 	free(line);
+
 	cmd = NULL;
 	line = NULL;
 }
@@ -55,13 +44,13 @@ void free_all(char **cmd, char *line)
  * @n: Size (How Much You Will Copy)
  *Return: Void Pointer
  */
-char *_memcpy(char *dest, char *src, unsigned int n)
+char *mem_cpy(char *dest, char *src, unsigned int o)
 {
-	unsigned int i;
+	unsigned int n;
 
-	for (i = 0; i < n; i++)
+	for (n = 0;n < o; n++)
 	{
-		dest[i] = src[i];
+		dest[n] = src[n];
 	}
 	return (dest);
 }
